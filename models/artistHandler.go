5da587b73aae9da1ede96188e25dc2dabcc318af
kevwasonga <kevinwasonga116@gmail.com>
2024-09-25 03:39:36 +0300

    redirect errors to error page

--- models/artistHandler.go
+++ models/artistHandler.go
@@ -11,20 +11,20 @@ import (
 func ArtistsHandler(w http.ResponseWriter, r *http.Request) {
 	artists, err := services.FetchAndUnmarshalArtists()
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		HandleError(w, err, http.StatusInternalServerError, "Error fetching artist data")
 		return
 	}
 
 	// Parse the HTML template file
 	tmpl, err := template.ParseFiles("templates/index.html")
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		HandleError(w, err, http.StatusInternalServerError, "Error loading index template")
 		return
 	}
 
 	// Execute the template with the artist data
 	err = tmpl.Execute(w, artists)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		HandleError(w, err, http.StatusInternalServerError, "Error rendering artist data")
 	}
 }
