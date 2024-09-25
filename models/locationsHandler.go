2e8d8956ec1c2611692240db596a8d063ce8097d
kevwasonga <kevinwasonga116@gmail.com>
2024-09-25 03:40:00 +0300

    redirect errors to error page

--- models/locationsHandler.go
+++ models/locationsHandler.go
@@ -14,29 +14,24 @@ type ArtistLocation struct {
 	Locations []string `json:"locations"`
 }
 
-// LocationsHandler serves the locations with artist names
 func LocationsHandler(w http.ResponseWriter, r *http.Request) {
-	// Fetch artist data
 	artists, err := services.FetchAndUnmarshalArtists()
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		HandleError(w, err, http.StatusInternalServerError, "Error fetching artist data")
 		return
 	}
 
-	// Create a map of artist IDs to names
 	artistNameMap := make(map[int]string)
 	for _, artist := range artists {
 		artistNameMap[artist.ID] = artist.Name
 	}
 
-	// Fetch locations data
 	locationsData, err := services.FetchAndUnmarshalLocations()
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		HandleError(w, err, http.StatusInternalServerError, "Error fetching locations data")
 		return
 	}
 
-	// Combine artist names with locations
 	var artistLocations []ArtistLocation
 	for _, loc := range locationsData.Index {
 		name, found := artistNameMap[loc.ID]
@@ -49,20 +44,14 @@ func LocationsHandler(w http.ResponseWriter, r *http.Request) {
 		})
 	}
 
-	// Parse the HTML template file
 	tmpl, err := template.ParseFiles(filepath.Join("templates", "locations.html"))
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		HandleError(w, err, http.StatusInternalServerError, "Error loading template")
 		return
 	}
 
-	// Execute the template with the combined data
-	err = tmpl.Execute(w, struct {
-		Index []ArtistLocation
-	}{
-		Index: artistLocations,
-	})
+	err = tmpl.Execute(w, struct{ Index []ArtistLocation }{Index: artistLocations})
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		HandleError(w, err, http.StatusInternalServerError, "Error rendering template")
 	}
 }
